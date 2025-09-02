8d8c2ddabd4a29500ce582388b4f4c2dfa4e3c66
Sheeeesh-code <E1570838@u.nus.edu>
2025-09-02 17:49:20 +0800

    Rename Duke to Amadeus and add skeleton

---
+++ src/main/java/amadeus/Amadeus.java
@@ -0,0 +1,31 @@
+package amadeus;
+
+public class Amadeus {
+    public static void main(String[] args) {
+        String logo =
+                "╔══════════════════════════════════════════════════════════════╗\n"
+                        + "║                                                              ║\n"
+                        + "║  █████╗ ███╗   ███╗ █████╗ ██████╗ ███████╗██╗   ██╗███████╗ ║\n"
+                        + "║ ██╔══██╗████╗ ████║██╔══██╗██╔══██╗██╔════╝██║   ██║██╔════╝ ║\n"
+                        + "║ ███████║██╔████╔██║███████║██║  ██║█████╗  ██║   ██║███████╗ ║\n"
+                        + "║ ██╔══██║██║╚██╔╝██║██╔══██║██║  ██║██╔══╝  ██║   ██║╚════██║ ║\n"
+                        + "║ ██║  ██║██║ ╚═╝ ██║██║  ██║██████╔╝███████╗╚██████╔╝███████║ ║\n"
+                        + "║ ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝ ╚═════╝ ╚══════╝ ║\n"
+                        + "║                                                              ║\n"
+                        + "╚══════════════════════════════════════════════════════════════╝\n";
+
+
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("────────────────────────────────────────────────────────────────");
+        System.out.println("\uD83D\uDC4B Greetings. I am Amadeus, your cognitive assistant.");
+        System.out.println("I monitor divergences in world lines… and sometimes your mistakes.");
+        System.out.println("What knowledge do you seek today?");
+        System.out.println();
+        System.out.println("────────────────────────────────────────────────────────────────");
+        System.out.println("System shutting down... awaiting next transmission.");
+        System.out.println("El Psy Kongroo.");
+
+
+    }
+}
